4946237176551e4dd7a52dd6bfb8662f647e6106
Doug March <dougmarch@gmail.com>
2017-09-29 11:39:17 -0400

    fix deploy error

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const config = {
+  API_BASE_URL: 'https://cdn.contentful.com',
+  API_SPACE_ID: 'bbwpgjv1jd8l',
+  API_TOKEN: '1d368a3878504119f6a075082be5665196d98efadf78884bbb314d7fd350dfcc'
+}
