d2831e52867753b1602f624e6af1b46181a91608
Doug March <dougmarch@gmail.com>
2017-10-04 11:36:30 -0400

    move navigation to header instead of in body of home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router'
+import { Box, Flex } from 'rebass'
 
 import { color } from '../../theme'
 
-const StyledHeader = styled.header`
+const StyledHeader = styled(Flex)`
   background-color: ${color.white};
-  text-align: center;
-  min-height: 100px;
-  padding-top: 40px;
   text-transform: uppercase;
-  color: ${color.gray6};
+`
+
+const StyledLink = styled(Link)`
+  padding-left: .5rem;
+  padding-right: .5rem;
 `
 
 class Header extends Component {
   render () {
     return (
-      <StyledHeader>
-        Header
+      <StyledHeader is='header'>
+        <Box pl={3} py={4}>
+          <StyledLink to={'shows'}>Doug Gillard</StyledLink>
+        </Box>
+        <Box pr={3} py={4} ml='auto'>
+          <StyledLink to={'shows'}>Upcoming Shows</StyledLink>
+          <StyledLink to={''}>Music</StyledLink>
+          <StyledLink to={''}>Discography</StyledLink>
+          <StyledLink to={'about'}>About</StyledLink>
+        </Box>
       </StyledHeader>
     )
   }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router'
-import { Heading } from 'rebass'
+import { Box, Heading, Text } from 'rebass'
 
 import HeroImgSrc from './hero.jpg'
 
@@ -21,13 +21,17 @@ const Hero = styled.div`
 const MusicEmbed = styled.div`
   min-height: 150px;
   padding-top: 75px;
+  margin-top: 20px;
   text-transform: uppercase;
   color: ${color.gray6};
   background-color: ${color.gray1};
 `
 
-const StyledHeading = styled(Heading)`
+const StyledSection = styled(Box)`
   border-bottom: 1px dotted ${color.gray2};
+`
+
+const StyledHeading = styled(Heading)`
   color: ${color.gray7};
 `
 
@@ -38,21 +42,43 @@ class Home extends Component {
         <Hero>
           test
         </Hero>
-        <MusicEmbed>
-          put music embed here
-        </MusicEmbed>
-        <StyledHeading py={3} f={4} is={'h2'}>
-          <Link to={'shows'}>Upcoming Shows</Link>
-        </StyledHeading>
-        <StyledHeading py={3} f={4} is={'h2'}>
-          <Link to={''}>Music</Link>
-        </StyledHeading>
-        <StyledHeading py={3} f={4} is={'h2'}>
-          <Link to={''}>Discography</Link>
-        </StyledHeading>
-        <StyledHeading py={3} f={4} is={'h2'}>
-          <Link to={'about'}>About</Link>
-        </StyledHeading>
+
+        <StyledSection py={3} is='section'>
+          <StyledHeading f={4} is={'h2'}>
+            Upcoming Shows
+          </StyledHeading>
+
+          <Text>FRI 10.06.17 - Lexington, KY - The Burl</Text>
+          <Text>SAT 10.07.17 - Morengo, OH - Hoof Hearted Brewing</Text>
+          <Text>FRI 10.13.17 - Durham, NC - Motorco Music Hall</Text>
+          <Text>SAT 10.14.17 - Columbia, SC - The Jam Room Music Festival</Text>
+
+          <Link>View all</Link>
+        </StyledSection>
+
+        <StyledSection py={3} is='section'>
+          <StyledHeading f={4} is={'h2'}>
+            Music
+          </StyledHeading>
+
+          <Text>Info about latest album</Text>
+
+          <Link>View discography</Link>
+
+          <MusicEmbed>
+            put music embed here
+          </MusicEmbed>
+        </StyledSection>
+
+        <StyledSection py={3} is='section'>
+          <StyledHeading f={4} is={'h2'}>
+            About
+          </StyledHeading>
+
+          <Text>Short bio, links to about us page</Text>
+
+          <Link>View discography</Link>
+        </StyledSection>
       </Wrapper>
     )
   }
